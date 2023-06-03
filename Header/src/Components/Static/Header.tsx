467bc848481e9fddab23c04ae596b97c81a0969f
Damilola Sanni <sannifortune11@gmail.com>
2023-06-03 09:49:14 +0100

    header

--- Header/src/Components/Static/Header.tsx
+++ Header/src/Components/Static/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
   };
   return (
     <div>
-      <div className="bg-black flex justify-center h-20">
+      <div className="bg-[lavender] flex justify-center h-20">
         <div className="w-[90%] text-white flex justify-between items-center h-[100%] ">
           <Logo>iModule Federation</Logo>
 
